d7c6ec3f5efa0f1f0d751622f02edf3906986ae2
AvaneeshPothuri <122687950+AvaneeshPothuri@users.noreply.github.com>
2023-08-14 11:18:45 +0530

    Add files via upload

--- OpenCV-ArUco-Marker-Detection/aruco_detect.py
+++ OpenCV-ArUco-Marker-Detection/aruco_detect.py
@@ -1,57 +1,71 @@
 import cv2
 
+# Constants for maximum display size, font scale, and text thickness
+MAX_DISPLAY_SIZE = 1500
+FONT_SCALE = 1.0
+TEXT_THICKNESS = 2
+
+# Define marker colors
+MARKER_COLORS = {
+    cv2.aruco.DICT_4X4_250: (0, 0, 255),
+    cv2.aruco.DICT_5X5_250: (0, 255, 0),
+    cv2.aruco.DICT_6X6_250: (255, 0, 0),
+    cv2.aruco.DICT_7X7_250: (255, 255, 0)
+}
+
 def detect_and_mark_markers(image_path):
     # Load the image
     image = cv2.imread(image_path)
 
-    # Define the ArUco dictionaries for 4X4, 5X5, 6X6, and 7X7 markers
-    aruco_dict_4x4 = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_250)
-    aruco_dict_5x5 = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_5X5_250)
-    aruco_dict_6x6 = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_250)
-    aruco_dict_7x7 = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_7X7_250)
+    # Define the ArUco dictionaries for different marker sizes
+    aruco_dict_4x4 = cv2.aruco.DICT_4X4_250
+    aruco_dict_5x5 = cv2.aruco.DICT_5X5_250
+    aruco_dict_6x6 = cv2.aruco.DICT_6X6_250
+    aruco_dict_7x7 = cv2.aruco.DICT_7X7_250
 
     # Create the detector parameters object
     parameters = cv2.aruco.DetectorParameters()
 
-    # Detect markers for each dictionary size
-    corners, ids, _ = cv2.aruco.detectMarkers(image, aruco_dict_4x4, parameters=parameters)
-    draw_markers(image, corners, ids, color=(0, 0, 255))  # Draw markers for 4x4 dictionary with blue color
+    # Detect markers for each dictionary size and draw them
+    detect_and_draw(image, aruco_dict_4x4, parameters)
+    detect_and_draw(image, aruco_dict_5x5, parameters)
+    detect_and_draw(image, aruco_dict_6x6, parameters)
+    detect_and_draw(image, aruco_dict_7x7, parameters)
 
-    corners, ids, _ = cv2.aruco.detectMarkers(image, aruco_dict_5x5, parameters=parameters)
-    draw_markers(image, corners, ids, color=(0, 255, 0))  # Draw markers for 5x5 dictionary with green color
+    # Resize the image and display the result
+    display_image(image)
 
-    corners, ids, _ = cv2.aruco.detectMarkers(image, aruco_dict_6x6, parameters=parameters)
-    draw_markers(image, corners, ids, color=(255, 0, 0))  # Draw markers for 6x6 dictionary with red color
+def detect_and_draw(image, aruco_dict, parameters):
+    corners, ids, _ = cv2.aruco.detectMarkers(image, cv2.aruco.getPredefinedDictionary(aruco_dict), parameters=parameters)
+    draw_markers(image, corners, ids, MARKER_COLORS[aruco_dict])
 
-    corners, ids, _ = cv2.aruco.detectMarkers(image, aruco_dict_7x7, parameters=parameters)
-    draw_markers(image, corners, ids, color=(255, 255, 0))  # Draw markers for 7x7 dictionary with yellow color
+def draw_markers(image, corners, ids, color):
+    if ids is not None:
+        for i in range(len(ids)):
+            if len(corners) > i:
+                current_corners = [corners[i]]
+                cv2.aruco.drawDetectedMarkers(image, current_corners, ids[i], borderColor=color)
+                org = (int(corners[i][0][0][0]), int(corners[i][0][0][1]))
+                cv2.putText(image, str(ids[i][0]), org, cv2.FONT_HERSHEY_SIMPLEX, FONT_SCALE, (0, 0, 0), TEXT_THICKNESS)
 
-    # Resize the image to fit the screen or make it larger if it's smaller
-    max_display_size = 1500
+def display_image(image):
+    # Resize the image to fit the screen or make it larger if needed
     height, width = image.shape[:2]
-    scale_factor = max_display_size / max(height, width)
-    if height > max_display_size or width > max_display_size:
-        image = cv2.resize(image, (int(width * scale_factor), int(height * scale_factor)))
+    scale_factor = MAX_DISPLAY_SIZE / max(height, width)
+    if height > MAX_DISPLAY_SIZE or width > MAX_DISPLAY_SIZE:
+        image = cv2.resize(image, None, fx=scale_factor, fy=scale_factor)
     else:
-        image = cv2.resize(image, (int(width * scale_factor), int(height * scale_factor)))
+        image = cv2.resize(image, None, fx=scale_factor, fy=scale_factor)
 
     # Display the resulting image with detected markers and IDs
     cv2.imshow("ArUco Marker Detection Result", image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-def draw_markers(image, corners, ids, color):
-    # Check if markers and IDs are detected
-    if ids is not None:
-        for i in range(len(ids)):
-            if len(corners) > i:
-                # Draw detected markers on the image
-                current_corners = [corners[i]]  # Get corners of the current marker
-                cv2.aruco.drawDetectedMarkers(image, current_corners, ids[i], borderColor=color)  # Draw marker outline
-                org = (int(corners[i][0][0][0]), int(corners[i][0][0][1]))  # Position for displaying marker ID
-                cv2.putText(image, str(ids[i][0]), org, cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 0), 2)  # Display marker ID
-
-
 if __name__ == "__main__":
-    sample_image_path = 'images/img2.ppm'  
-    detect_and_mark_markers(sample_image_path)
+    # List of sample image paths
+    sample_image_paths = ['images/img1.png', 'images/img2.ppm', 'images/img3.jpg','images/img4.png','images/img5.png']
+    
+    # Process and display each sample image
+    for image_path in sample_image_paths:
+        detect_and_mark_markers(image_path)
